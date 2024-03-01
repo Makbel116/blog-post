fd15269806d898c0abe319e29533572c9bc8dc65
unknown <makbel9395@gmail.com>
2024-03-01 04:14:04 -0800

    starts with the basic structure of the website

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const ejs=require("ejs");
+const bodyParser=require("body-parser");
+
+const app=express();
+
+app.set("view engine","ejs");
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.listen(3000,function(req,res){
+    console.log("server is listening.")
+})
