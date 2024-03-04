63603839b35623f6176a8d606666f23fc0823363
unknown <makbel9395@gmail.com>
2024-03-04 00:38:39 -0800

    works on post  page

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
-
+const _ = require("lodash");
 
 const app = express();
 const homeStartingContent =
@@ -10,8 +10,7 @@ const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts=[];
-
+const posts = [];
 
 app.set("view engine", "ejs");
 
@@ -19,30 +18,41 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.render("home",{homeStartingContent:homeStartingContent,posts:posts});
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts,
+  });
 });
 
 app.get("/about", function (req, res) {
-  res.render("about",{aboutContent:aboutContent});
+  res.render("about", { aboutContent: aboutContent });
 });
 
 app.get("/contact", function (req, res) {
-  res.render("contact",{contactContent:contactContent});
+  res.render("contact", { contactContent: contactContent });
 });
 
-app.get("/compose",function(req,res){
-  res.render("compose")
-})
-app.post("/compose",function(req,res){
-let compose_obj={
-  title:req.body.title,
-  post:req.body.post
-};
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+app.post("/compose", function (req, res) {
+  let compose_obj = {
+    title: req.body.title,
+    post: req.body.post,
+  };
   posts.push(compose_obj);
   res.redirect("/");
+});
+
+app.get("/post/:posts",function(req,res){
+  let requstsParam=_.kebabCase(req.params.posts);
+ posts.forEach(post => {
+  let postTitle=_.kebabCase(post.title);
+  if (postTitle===requstsParam){
+    res.render("post",{postTitle:postTitle,postContent:post.post})
+  }
+ });
 })
-
-
 app.listen(3000, function (req, res) {
   console.log("server is listening.");
 });
